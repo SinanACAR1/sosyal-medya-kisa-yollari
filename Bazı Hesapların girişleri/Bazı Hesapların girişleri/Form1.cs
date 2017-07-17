d3a683b273800e20e3198c94af86d6a4cbf65da0
Sinan ACAR <sinanacar14654@gmail.com>
2017-07-17 21:06:48 +0300

    Add files via upload

---
+++ Bazı Hesapların girişleri/Bazı Hesapların girişleri/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Bazı_Sosyal_Medya_kısaYolları
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Show();
+            f2.webBrowser1.Navigate("https://www.facebook.com/");
+            f2.webBrowser1.ScriptErrorsSuppressed = true;
+            f2.Text = "Facebook";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Show();
+            f2.webBrowser1.Navigate("https://twitter.com/");
+            f2.webBrowser1.ScriptErrorsSuppressed = true;
+            f2.Text = "Twitter";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Show();
+            f2.webBrowser1.Navigate("https://accounts.google.com/ServiceLogin?service=mail&passive=true&rm=false&continue=https://mail.google.com/mail/&ss=1&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1#identifier");
+            f2.webBrowser1.ScriptErrorsSuppressed = true;
+            f2.Text = "Gmail";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Show();
+            f2.webBrowser1.Navigate("https://www.microsoft.com/tr-tr/outlook-com/?cb=v8ho");
+            f2.webBrowser1.ScriptErrorsSuppressed = true;
+            f2.Text = "Outlook";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Show();
+            f2.webBrowser1.Navigate("https://login.live.com/tr");
+            f2.webBrowser1.ScriptErrorsSuppressed = true;
+            f2.Text = "Microsoft";
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Show();
+            f2.webBrowser1.Navigate("http://www.msn.com/tr-tr");
+            f2.webBrowser1.ScriptErrorsSuppressed = true;
+            f2.Text = "Msn";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Bazı Hesapların girişleri/Bazı Hesapların girişleri/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Bazı_Sosyal_Medya_kısaYolları
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
